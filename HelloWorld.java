4464eb0525eace017cf9a187227f5a6f3734918c
helenayounes <younesq.helena@gmail.com>
2016-01-24 20:28:07 -0700

    ajout commentaire

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,10 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Please enter a word or phrase");
     Scanner input=new Scanner(System.in);
-    String word = input.next(); 
+    String word = input.next();
 
     while ( word.length() > 0 )  {
-
+//study while loop
       for (int i=0; i<word.length();i++) {
         char lettre = word.charAt(i);
 
