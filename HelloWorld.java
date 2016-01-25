6e7f800fd788a803e7b40574161aaa79fa82da68
remyyounes <ryounes@gmail.com>
2016-01-24 19:08:55 -0800

    Loop over words

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
 public class HelloWorld {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Please enter a word or phrase");
     Scanner input=new Scanner(System.in);
-    String h=input.next();
+    String word = input.next(); //he
+    // he le na
+    while ( word.length() > 0 )  {
 
-    for (int i=0; i<h.length();i++){
-      char lettre = h.charAt(i);
+      for (int i=0; i<word.length();i++) {
+        char lettre = word.charAt(i);
 
-      if( lettre == 'a' ){
-        lettre = '-';
+        if( lettre == 'a' ) {
+          lettre = '\0';
+        }
+        if( lettre == 'e' ) {
+          lettre = '\0';
+        }
+        if( lettre == 'i' ) {
+          lettre = '\0';
+        }
+        if( lettre == 'o' ) {
+          lettre = '\0';
+        }
+        if( lettre == 'u' ) {
+          lettre = '\0';
+        }
+        if( lettre == 'y' ) {
+          lettre = '\0';
+        }
+        System.out.print(lettre);
       }
-      if( lettre == 'e' ){
-        lettre = '-';
-      }
-      if( lettre == 'i' ){
-        lettre = '-';
-      }
-      if( lettre == 'o' ){
-        lettre = '-';
-      }
-      if( lettre == 'u' ){
-        lettre = '-';
-      }
-      if( lettre == 'y' ){
-        lettre = '-';
-      }
-
-      System.out.println(lettre);
+      word = input.next();
     }
   }
 }
